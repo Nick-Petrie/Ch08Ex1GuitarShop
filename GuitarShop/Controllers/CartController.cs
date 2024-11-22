333672e20ac222184bb9790c2153b151ac04a420
Nick Petrie <nickolaspetrie@gmail.com>
2024-11-22 15:56:51 -0500

    Add project files.

---
+++ GuitarShop/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(string id)
+        {
+            TempData["message"] = $"Product '{id}' has been added to the cart.";
+            return RedirectToAction("List", "Product");
+        }
+    }
+}
